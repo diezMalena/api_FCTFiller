73dd49f0702144ab70d4b4f6be0bf5e75e8707a7
David Sánchez Barragán <davidsanchezbarragan@gmail.com>
2022-01-20 10:02:01 +0100

    Commit inicial

---
+++ app/Http/Controllers/ControladorAlumnos/ControladorAlumno.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ControladorAlumnos;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ControladorAlumno extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/ControladorGenerico.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControladorGenerico extends Controller
+{
+    //
+}
