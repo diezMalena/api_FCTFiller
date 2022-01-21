23c5820d742560a907e1c9bde55712f8180bd4a1
DaniJCoello <daniel.jimenezcoello@gmail.com>
2022-01-21 09:24:09 +0100

    Empezado método de generación de anexo 0 Creada estructura carpetas en public Añadido anexo0.docx a public

--- app/Http/Controllers/ContrladoresDocentes/ControladorTutorFCT.php
+++ app/Http/Controllers/ContrladoresDocentes/ControladorTutorFCT.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\ContrladoresDocentes;
 
 use App\Http\Controllers\Controller;
+use App\Models\CentroCiclo;
 use Illuminate\Http\Request;
+use PhpOffice\PhpWord\TemplateProcessor;
 
 class ControladorTutorFCT extends Controller
 {
-    //
+
+    /**
+     * Genera el Anexo 0, convenio entre una empresa y un centro
+     * @param string $dniTutor el DNI del tutor que está loggeado en el sistema
+     * @param string $cifEmpresa el CIF de la empresa con la que se hará el convenio
+     *
+     * @author @DaniJCoello
+     */
+    public function generarAnexo0(string $dniTutor, string $cifEmpresa) {
+        $nombreArchivo = 'anexo0';
+        $rutaArchivo = 'anexos/plantillas/' . $nombreArchivo . '.docx';
+        $template = new TemplateProcessor($rutaArchivo);
+
+
+    }
+
 }
