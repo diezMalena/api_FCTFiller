de45d76b8bd848a4f48062542908a8b76ffefda0
Alvaro <alvarosantosmartin6@gmail.com>
2022-01-25 14:50:35 +0100

    fallo

--- app/Http/Controllers/ContrladoresDocentes/ControladorTutorFCT.php
+++ app/Http/Controllers/ContrladoresDocentes/ControladorTutorFCT.php
@@ -3,9 +3,64 @@
 namespace App\Http\Controllers\ContrladoresDocentes;
 
 use App\Http\Controllers\Controller;
+use App\Models\Alumno;
+use App\Models\Curso;
+use App\Models\Empresa;
+use App\Models\EmpresaCurso;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Foreach_;
 
 class ControladorTutorFCT extends Controller
 {
     //
+    /**
+     *  Esta función se encarga de coger los datos dni y nombre de los
+     *  alumnos asignados al dni del tutor que recibimos como parámetro.
+     *
+     *  @author
+     *  @param $dni es el dni del tutor
+     */
+    public function solicitarAlumnosSinEmpresa($dni){
+        $cod_curso = Curso::select('cod_curso')->where([
+            ['dni_tutor', $dni],
+        ])->get();
+        $alumnos = Alumno::select('dni','nombre')->where([
+            ['cod_curso', $cod_curso],
+        ])->get();
+        return response()->json($alumnos, 200);
+    }
+    /**
+     *  Esta función se encarga de coger los datos dni, nombre y cif de los
+     *  alumnos asignados al dni del tutor que recibimos como parámetro.
+     *
+     *  @author
+     *  @param $dni es el dni del tutor
+     */
+    public function solicitarEmpresas($dni){
+        $cod_curso = Curso::select('cod_curso')->where([
+            ['dni_tutor', $dni],
+        ])->get();
+        $cif = EmpresaCurso::select('cif_empresa')->where([
+            ['cod_curso', $cod_curso],
+        ])->get();
+        $empresas = Empresa::select('nombre','cif')->where([
+            ['cif', $cif],
+        ])->get();
+        return response()->json($empresas, 200);
+    }
+    /**
+     *  Esta función se encarga de actualizar la empresa a la que están asignados
+     *  los alumnos.
+     *
+     *  @author
+     *  @param $empresas son las empresas
+     */
+    public function actualizarEmpresaAsignadaAlumno($empresas){
+
+        foreach ($empresas as $empresa => $value) {
+            # code...
+        }
+
+        return response()->json($empresas, 200);
+    }
 }
---
+++ database/migrations/2022_01_23_184129_empresa_alumno.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EmpresaAlumno extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa_alumno', function (Blueprint $table) {
+            $table->string('id_empresa');
+            $table->string('dni_alumno');
+            $table->string('curso_academico');
+            $table->primary(['id_empresa', 'dni_alumno']);
+            $table->foreign('id_empresa')->references('id')->on('empresa')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('dni_alumno')->references('dni')->on('alumno')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
