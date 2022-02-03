b1b121025f147436387765fafad1ca5712046c9f
Malena <malena10vinas@gmail.com>
2022-02-03 19:32:58 +0100

    Modelo y migration de Seguimiento creados.

---
+++ app/Models/Seguimiento.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Migración correspondiente: seguimientos
+ * Contiene los datos de las jornadas diarias que realizan los alumnos en las empresas.
+ * @author Malena.
+ */
+
+class Seguimiento extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id_empresa',
+        'dni_alumno',
+        'fecha_jornada', //Cuando el alumno hizo las actividades
+        'actividades',
+        'observaciones',
+        'tiempo_empleado'
+    ];
+
+    protected $table = 'seguimiento';
+}
