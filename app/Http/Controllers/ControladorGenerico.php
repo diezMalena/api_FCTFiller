719b388fd250e726e7dce86f4e12d63655fb6481
Alvaro <alvarosantosmartin6@gmail.com>
2022-02-17 11:03:53 +0100

    creada la ruta

--- app/Http/Controllers/ControladorGenerico.php
+++ app/Http/Controllers/ControladorGenerico.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class ControladorGenerico extends Controller
 {
 
+    public function login(Request $request)
+    {
+        $email = $request->get('email');
+        $pass = $request->get('pass');
+
+        error_log(print_r($email, true));
+        error_log(print_r($pass, true));
+    }
 }
