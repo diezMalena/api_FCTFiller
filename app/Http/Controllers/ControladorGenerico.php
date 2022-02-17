a5accaaa9d743e5fd466cc1b94ccf195b94cdb28
Alvaro <alvarosantosmartin6@gmail.com>
2022-02-17 11:11:00 +0100

    preparada la query

--- app/Http/Controllers/ControladorGenerico.php
+++ app/Http/Controllers/ControladorGenerico.php
@@ -9,9 +9,22 @@ class ControladorGenerico extends Controller
 
     public function login(Request $request)
     {
+
         $email = $request->get('email');
         $pass = $request->get('pass');
 
+        $quer = 'select alumno.nombre, alumno.dni from alumno, curso, alumno_curso where alumno.dni not in'
+            . ' (select alumno.dni from alumno, curso, alumno_curso, empresa_alumno'
+            . ' WHERE curso.cod_curso = alumno_curso.cod_curso'
+            . ' AND alumno.dni = alumno_curso.dni'
+            . ' AND curso.dni_tutor = ?'
+            . ' AND alumno_curso.dni = empresa_alumno.dni_alumno)'
+            . ' AND alumno.dni = alumno_curso.dni'
+            . ' AND alumno_curso.cod_curso = curso.cod_curso'
+            . ' AND alumno.va_a_fct != 0'
+            . ' AND curso.dni_tutor = ?';
+        $alumnos = DB::select($quer, [$dni, $dni]);
+        return response()->json($alumnos, 200);
         error_log(print_r($email, true));
         error_log(print_r($pass, true));
     }
