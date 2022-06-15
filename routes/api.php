ee0aab6fae5b8e27e972a30230e09e874fae0334
DaniJCoello <daniel.jimenezcoello@gmail.com>
2022-06-15 23:57:09 +0200

    Revert "test auth produ"
    This reverts commit 60115f8c2a6493f60064d500f9d087eef8eba4aa.

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::group(['middleware' => ['Cors']], function () {
 | Rutas genéricas
 |--------------------------------------------------------------------------
 */
-Route::group(['middleware' => ['Cors', 'auth:api']], function () {
+Route::group(['middleware' => ['Cors']], function () {
     /*********************Obtener provincias y ciudades*********************/
     Route::get('listarProvincias', [ControladorGenerico::class, 'listarProvincias']);
     Route::get('listarCiudades/{provincia}', [ControladorGenerico::class, 'listarCiudades']);
